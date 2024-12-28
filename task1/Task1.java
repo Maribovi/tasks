09978951c050df4948189e742993b5271b293d6d
Maria Tretiakova <mailerbowler@yandex.ru>
2024-12-28 19:57:29 +0300

    Update Task1.java

--- task1/Task1.java
+++ task1/Task1.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 
 public class Task1 {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите значение n (количество элементов массива): ");
+        int n = scanner.nextInt();
+        System.out.print("Введите значение m (интервал): ");
+        int m = scanner.nextInt();
 
-            System.out.print("Введите значение n (количество элементов массива: ");
-            int n = scanner.nextInt();
-            System.out.print("Введите значение m (интервал): ");
-            int m = scanner.nextInt();
-
-          int j = 1;
-            int[] myArray = new int[n];
-               for (int i = 0; i<n; i++) {
-                   myArray[i] = j++;
-                   }
-
-            StringBuilder path = new StringBuilder();
-            int x = 0;
-            do {
-                path.append(myArray[x]);
-                x = (x + m-1) % n;
-            } while (x != 0);
-
-            System.out.println("Полученный путь: " + path);
+       int j = 1;
+        int[] myArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            myArray[i] = j++;
         }
+
+        int current = 0;
+        System.out.print("Путь: ");
+        do {
+            System.out.print(myArray[current]);
+            current = (current + m-1) % n;
+        } while (current != 0);
+        scanner.close();
+    }
 }
+
 
 
 
